e71ce0c0704515f1c1ba95856d8df0210c3b2e03
Ramakrishnan K <NL100455@NL.gd>
2026-03-07 15:16:30 +0530

    Fix Railway PostgreSQL connection

--- NPI_ACS_Web/Program.cs
+++ NPI_ACS_Web/Program.cs
@@ -7,9 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services
 builder.Services.AddControllersWithViews();
 
+// PostgreSQL connection
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 {
@@ -37,7 +39,6 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-// MAIN ROUTING
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=ACSTasks}/{action=Index}/{id?}");
